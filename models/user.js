a9fac07991ac108f89fc59af5fd3c1a81e1d5400
sunshine-web-coder <just4fun11021102@gmail.com>
2023-10-15 07:37:41 +0100

    updating the user model

--- models/user.js
+++ models/user.js
@@ -1,27 +1,34 @@
-const mongoose = require ('mongoose');
-const bcrypt = require ('bcrypt');
-const jwt = require ('jsonwebtoken');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const userSchema = new mongoose.Schema (
-  {
-    name: String,
-    gender: String,
-    email: String,
-    phoneNumber: String,
-    dateOfBirth: Date,
-    password: String,
-    accountType: String,
-    currencyType: String,
-    country: String,
-    state: String,
-    homeAddress: String,
-    accountNumber: Number, // Unique account number
-    accountBalance: Number,
-    isAdmin: Boolean, // Field to indicate admin status
-  },
-  {
+const userSchema = new mongoose.Schema({
+  name: String,
+  gender: String,
+  email: String,
+  phoneNumber: String,
+  dateOfBirth: Date,
+  password: String,
+  accountType: String,
+  currencyType: String,
+  country: String,
+  state: String,
+  homeAddress: String,
+  accountNumber: Number, // Unique account number
+  accountBalance: Number,
+  isAdmin: Boolean, // Field to indicate admin status
+},{
     timestamps: true,
-  }
-);
+  });
 
-module.exports = mongoose.model ('User', userSchema);
+// Hash the password before saving it
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const hash = await bcrypt.hash(this.password, 10); // Hash the password with a salt factor of 10
+  this.password = hash;
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
