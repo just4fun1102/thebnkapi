07b24575d6b4bf1adc9fa1775b548871a7d323cc
sunshine-web-coder <just4fun11021102@gmail.com>
2023-10-15 07:34:32 +0100

    updating the user model

--- models/user.js
+++ models/user.js
@@ -1,42 +1,27 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const mongoose = require ('mongoose');
+const bcrypt = require ('bcrypt');
+const jwt = require ('jsonwebtoken');
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  gender: String,
-  email: String,
-  phoneNumber: String,
-  dateOfBirth: Date,
-  password: String,
-  accountType: String,
-  currencyType: String,
-  country: String,
-  state: String,
-  homeAddress: String,
-  accountNumber: Number, // Unique account number
-  accountBalance: Number,
-  isAdmin: Boolean, // Field to indicate admin status
-},{
+const userSchema = new mongoose.Schema (
+  {
+    name: String,
+    gender: String,
+    email: String,
+    phoneNumber: String,
+    dateOfBirth: Date,
+    password: String,
+    accountType: String,
+    currencyType: String,
+    country: String,
+    state: String,
+    homeAddress: String,
+    accountNumber: Number, // Unique account number
+    accountBalance: Number,
+    isAdmin: Boolean, // Field to indicate admin status
+  },
+  {
     timestamps: true,
-  });
-
-// Hash the password before saving it
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
   }
-  const hash = await bcrypt.hash(this.password, 10); // Hash the password with a salt factor of 10
-  this.password = hash;
-  next();
-});
+);
 
-// Generate a JWT token for the user
-userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, 'your-secret-key', {
-    expiresIn: '1h', // Token expiration time
-  });
-  return token;
-};
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model ('User', userSchema);
